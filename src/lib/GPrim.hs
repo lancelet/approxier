ec679c7eaf1c725e54bac2ef809754bde9e3e59a
Jonathan Merritt <j.s.merritt@gmail.com>
2015-08-01 21:00:29 +1000

    Add remaining quadric surface primitives

--- src/lib/GPrim.hs
+++ src/lib/GPrim.hs
@@ -9,7 +9,7 @@ Stability   : experimental
 Geometric primitive definitions.
 -}
 module GPrim (
-  -- * Types
+  -- * Quadric surface primitives.
     Sphere (
       Sphere
     , sphereRadius
@@ -17,12 +17,103 @@ module GPrim (
     , sphereZMax
     , spherePhiMax
     )
+  , Cone (
+      Cone
+    , coneHeight
+    , coneRadius
+    , conePhiMax
+    )
+  , Cylinder(
+      Cylinder
+    , cylinderRadius
+    , cylinderZMin
+    , cylinderZMax
+    , cylinderPhiMax
+    )
+  , Hyperboloid(
+      Hyperboloid
+    , hyperboloidPoint1
+    , hyperboloidPoint2
+    , hyperboloidPhiMax
+    )
+  , Paraboloid(
+      Paraboloid
+    , paraboloidRMax
+    , paraboloidZMin
+    , paraboloidZMax
+    , paraboloidPhiMax
+    )
+  , Disk (
+      Disk
+    , diskHeight
+    , diskRadius
+    , diskPhiMax
+    )
+  , Torus (
+      Torus
+    , torusMajorRadius
+    , torusMinorRadius
+    , torusThetaMin
+    , torusThetaMax
+    , torusPhiMax
+    )
   ) where
+
+import VecMath (Point3)
+
+----------------------------------------------------------------------------------------------------
+-- QUADRICS
 
 -- |Sphere.
 data Sphere = Sphere
-  { sphereRadius :: !Float  -- ^ radius
-  , sphereZMin   :: !Float  -- ^ minimum z value
-  , sphereZMax   :: !Float  -- ^ maximum z value
-  , spherePhiMax :: !Float  -- ^ angle about z-axis, measured from zero at +x (degrees)
+  { sphereRadius :: !Float
+  , sphereZMin   :: !Float
+  , sphereZMax   :: !Float
+  , spherePhiMax :: !Float
+  } deriving (Show)
+
+-- |Cone.
+data Cone = Cone
+  { coneHeight :: !Float
+  , coneRadius :: !Float
+  , conePhiMax :: !Float
+  } deriving (Show)
+
+-- |Cylinder.
+data Cylinder = Cylinder
+  { cylinderRadius :: !Float
+  , cylinderZMin   :: !Float
+  , cylinderZMax   :: !Float
+  , cylinderPhiMax :: !Float
+  } deriving (Show)
+
+-- |Hyperboloid.
+data Hyperboloid = Hyperboloid
+  { hyperboloidPoint1 :: !Point3
+  , hyperboloidPoint2 :: !Point3
+  , hyperboloidPhiMax :: !Float
+  } deriving (Show)
+
+-- |Paraboloid.
+data Paraboloid = Paraboloid
+  { paraboloidRMax   :: !Float
+  , paraboloidZMin   :: !Float
+  , paraboloidZMax   :: !Float
+  , paraboloidPhiMax :: !Float
+  } deriving (Show)
+
+-- |Disk.
+data Disk = Disk
+  { diskHeight :: !Float
+  , diskRadius :: !Float
+  , diskPhiMax :: !Float
+  } deriving (Show)
+
+-- |Torus.
+data Torus = Torus
+  { torusMajorRadius :: !Float
+  , torusMinorRadius :: !Float
+  , torusThetaMin    :: !Float
+  , torusThetaMax    :: !Float
+  , torusPhiMax      :: !Float
   } deriving (Show)
